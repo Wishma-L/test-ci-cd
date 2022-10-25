3aa8b380813b12dc16c56a86549dc2f2bd6ad981
Wishma Herath <wishma.herath@clouda.io>
2022-10-25 20:47:38 +0530

    Hotfox

--- src/pages/custom-page.tsx
+++ src/pages/custom-page.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import type { PageProps } from "gatsby"
 // best way to build hard-coded page
-type CustomPageProps = {}
 
-const CustomPage = ({}: CustomPageProps) => {
+const CustomPage = () => {
     return (
       <>
         <h1>React TS FC Component</h1>
