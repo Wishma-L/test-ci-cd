475dbcfb5a05b3ad90774a9375257cc551071b21
Wishma Herath <wishma.herath@clouda.io>
2022-10-28 10:57:03 +0530

    testing husky

---
+++ src/pages/Err-page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrPage = () => {
+    return (
+      <>
+        <h1>The Second custom page</h1>
+        <div>List
+          
+        </div>
+      </>
+    )
+  };
+
+export default ErrPage
